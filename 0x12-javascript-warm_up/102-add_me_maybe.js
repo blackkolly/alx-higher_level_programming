342bca647d035e6e39f00f81d6fd09c52775b4f8
Oladipupo kola <komolafe.solomon@yahoo.com>
2022-09-05 14:28:50 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-exports.addMeMaybe = function (number, theFunction) {
-  const n = number + 1;
-  theFunction(n);
-};
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const ls = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const i in ls) {
-  console.log(ls[i]);
-}
